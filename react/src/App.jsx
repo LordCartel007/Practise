7659e95c83cd8fd0a3b9568ba06be749b3729f08
LordCartel007 <cartellord77@gmail.com>
2025-06-13 09:00:27 +0100

    update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -23,25 +23,91 @@
 
 // export default InputLogger;
 
-import React, { useState } from "react";
+// import React, { useState, useEffect } from 'react';
 
-function Typing() {
-  const [text, setText] = useState("");
+// function Counter() {
+//   const [count, setCount] = useState(0);
 
-  const handleChange = (e) => {
-    setText(e.target.value);
-    console.log(e.target.value);
-  };
+//   useEffect(() => {
+//     console.log('Count changed:', count);
+//   }, [count]); // run every time `count` changes
 
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//   );
+// }
+
+// import React, { useState, useEffect } from "react";
+// import Greetings from "./components/Greetings";
+
+// function add() {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     console.log("another added");
+//   }, [count]);
+//   function Welcome({ house, city }) {
+//     return (
+//       <h2>
+//         {" "}
+//         Lord Of the north from {city} , from the house of {house}
+//       </h2>
+//     );
+//   }
+//   return (
+//     <>
+//       <h1> count : {count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//       <h1>
+//         {" "}
+//         <Greetings name="tom" />
+//       </h1>
+//       <>
+//         <Welcome house="el" city="winterfell" />
+//       </>
+//     </>
+//   );
+// }
+
+// export default add;
+
+import React, { useState, useEffect } from "react";
+
+function userList() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) throw new Error("failed to fetch");
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchUser();
+  }, []);
   return (
     <>
-      <div>Type here</div>
-      <input
-        placeholder="type here"
-        value={text}
-        onChange={handleChange}
-      ></input>
+      <div>
+        <h2>Users:</h2>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
-export default Typing;
+
+export default userList;
---
+++ react/src/components/Greetings.jsx
@@ -0,0 +1,5 @@
+function Greetings({ name }) {
+  return <h1> Hello , {name} !</h1>;
+}
+
+export default Greetings;
